3eec0a2eee8f7b86a5f5764cc900ea174330daa2
Mota-aln <andrelu.mota@gmail.com>
2025-05-11 17:15:32 -0300

    Add files via upload

---
+++ Programa bugado.py
@@ -0,0 +1,10 @@
+capital = float(input("Informe, em reais, seu capital: "))
+juros = float(input("Informe, em porcentagem, os juros: "))
+tempo = float(input("Informe, em meses, a duração da aplicação: "))
+montante = 0
+juros=juros/100
+contador = 0
+for contador < tempo:
+    contador+=1
+  montante = capital * ((1+juros)**contador)
+  print(f"Mês {contador}, montante = R${montante:.2f}")
