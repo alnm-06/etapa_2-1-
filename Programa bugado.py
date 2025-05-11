f0f0bf20fd354ecf654b4f3cef6722954eb7ed29
André Luís <andre.nmota@sempreceub.com>
2025-05-11 17:35:36 -0300

    Update Programa bugado.py

--- Programa bugado.py
+++ Programa bugado.py
@@ -4,7 +4,7 @@ tempo = float(input("Informe, em meses, a duração da aplicação: "))
 montante = 0
 juros=juros/100
 contador = 0
-for contador < tempo:
+while contador < tempo:
     contador+=1
-  montante = capital * ((1+juros)**contador)
-  print(f"Mês {contador}, montante = R${montante:.2f}")
+    montante = capital * ((1+juros)**contador)
+    print(f"Mês {contador}, montante = R${montante:.2f}")
